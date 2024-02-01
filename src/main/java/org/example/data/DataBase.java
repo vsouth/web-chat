4d5903a8a24583aaebd8b2c8c9f7462a02c537fc
vsouth <neppchela@gmail.com>
2024-02-01 17:10:46 +0300

    database implementation

--- src/main/java/org/example/data/DataBase.java
+++ src/main/java/org/example/data/DataBase.java
@@ -1,12 +1,23 @@
 package org.example.data;
 
+import java.util.HashMap;
+
 public final class DataBase {
+    private static HashMap<String, User> users = new HashMap<>();
 
     private DataBase() {
     }
 
+
     public static void init() {
         System.out.println("Установка соединения с Базой Данных");
+        users.put("admin", new User("admin", "admin", "admin", UserType.ADMIN));
+        users.put("fox", new User("fox", "fox", "fox", UserType.CLIENT));
+        users.put("bird", new User("bird", "bird", "bird", UserType.CLIENT));
+    }
+
+    public static HashMap<String, User> getUsers() {
+        return users;
     }
 
     public static void close() {
