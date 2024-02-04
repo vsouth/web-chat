2087f4af56da8ff80ebaa82d6ec6014c81e8d55b
vsouth <neppchela@gmail.com>
2024-02-04 17:46:27 +0300

    added admin filter

---
+++ src/main/java/org/example/filter/AdminFilter.java
@@ -0,0 +1,44 @@
+package org.example.filter;
+
+import org.example.data.User;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+import static org.example.Resources.COMMAND_SHOW_CHAT_PAGE;
+import static org.example.Resources.COMMAND_SHOW_LOGIN_PAGE;
+import static org.example.data.UserType.ADMIN;
+
+public class AdminFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        HttpSession session = httpRequest.getSession();
+
+        if (session == null || session.getAttribute("user") == null) {
+            httpResponse.sendRedirect(COMMAND_SHOW_CHAT_PAGE);
+            return;
+        }
+        User user = (User) session.getAttribute("user");
+        if (user.getUserType() != ADMIN) {
+            httpResponse.sendRedirect(COMMAND_SHOW_CHAT_PAGE);
+            return;
+        }
+        filterChain.doFilter(request, response);
+
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
